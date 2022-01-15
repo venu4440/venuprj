e63b531c645f25f6c08b8ec9ea2d9b199b290f18
venu4440 <venu.salakapuram@yahoo.com>
2022-01-15 21:16:51 +0530

    changed code

--- server.js
+++ server.js
@@ -1,37 +1,15 @@
-var express = require('express'),
-   path = require('path'),
-   fs = require('fs');
-var compression = require('compression');
-var app = express();
-var staticRoot = __dirname + '/dist/venungprj/';
-var env = process.env.NODE_ENV || 'development';
+//Install express server
+const express = require('express');
+const path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
+const app = express();
 
-app.use(compression());
-/* other middleware */
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/venungprj'));
 
-/* place any backend routes you have here */    
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/venungprj/'}),
+);
 
-app.use(function(req, res, next) {
-    //if the request is not html then move along
-    var accept = req.accepts('html', 'json', 'xml');
-    if (accept !== 'html') {
-        return next();
-    }
-
-    // if the request has a '.' assume that it's for a file, move along
-    var ext = path.extname(req.path);
-    if (ext !== '') {
-        return next();
-    }
-
-    fs.createReadStream(staticRoot + 'index.html').pipe(res);
-
-});
-
-app.use(express.static(staticRoot));
-
-app.listen(app.get('port'), function() {
-    console.log('app running on port', app.get('port'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
